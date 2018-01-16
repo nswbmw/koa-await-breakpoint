ea55245a351f72d6886f1c7d18091b96b34f3740
nswbmw <gxqzk@126.com>
2018-01-16 23:40:31 +0800

    update example

--- example/routes/users.js
+++ example/routes/users.js
@@ -1,8 +1,38 @@
-const User = require('../models/users')
+const Mongolass = require('mongolass')
+const mongolass = new Mongolass('mongodb://localhost:27017/test')
+const User = mongolass.model('User')
+const Post = mongolass.model('Post')
+const Comment = mongolass.model('Comment')
 
-exports.createUser = async function createUser (ctx) {
+exports.createUser = async function (ctx) {
   const name = ctx.query.name || 'default'
   const age = +ctx.query.age || 18
-  await User.createUser(name, age)
+  await createUser(name, age)
   ctx.status = 204
+}
+
+async function createUser (name, age) {
+  const user = (await User.create({
+    name,
+    age
+  })).ops[0]
+  await createPost(user)
+}
+
+async function createPost (user) {
+  const post = (await Post.create({
+    uid: user._id,
+    title: 'post',
+    content: 'post'
+  })).ops[0]
+
+  await createComment(user, post)
+}
+
+async function createComment (user, post) {
+  await Comment.create({
+    userId: user._id,
+    postId: post._id,
+    content: 'comment'
+  })
 }
