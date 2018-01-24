ded2f7338bf0d22d70b205accefa7af61dd42237
nswbmw <gxqzk@126.com>
2018-01-24 23:45:32 +0800

    update example

--- example/app.js
+++ example/app.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const koaYieldBreakpoint = require('..')({
+const koaAwaitBreakpoint = require('..')({
   name: 'api',
   files: [path.join(__dirname, '**/*.js')]
 })
@@ -8,7 +8,7 @@ const Koa = require('koa')
 const route = require('koa-route')
 const app = new Koa()
 
-app.use(koaYieldBreakpoint)
+app.use(koaAwaitBreakpoint)
 app.use(route.post('/users', require('./routes/users').createUser))// curl -XPOST localhost:3000/users
 
 app.listen(3000, () => {
